32776018b5518557a28f7d2d509a0fe8d3526abf
Mohana Misra <iammohanamisra@gmail.com>
2022-11-07 12:39:31 +0530

    Added grid resize functionality with user input

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let grid = document.createElement("div");
-grid.className = "grid";
-let boxToColour;
+let grid = document.getElementById("grid");
+let cellNum = 0;
 let mouseDown = false;
 
 function createBox(){
@@ -18,16 +17,21 @@ function createGrid(cellNum){
             column.appendChild(box);
             box.addEventListener('mousedown', (e) => {
                 e.preventDefault();
+                // This line prevents unexpected behaviour caused due to mousedown's default drag functionality;
                 mouseDown = true;
+                //tells the document 'pen is down'.
             });
             box.addEventListener('mouseup', (e)=>{
                 mouseDown = false;
+                //tells the document 'pen is up'.
             });
             box.addEventListener('mouseover', (e)=>{
                 hovered(e.target);
+                //if the box is hovered over then you pass that div to hovered and colour it. But hovered only colours if    mouseDown is true, i.e., only if th eprevious event listener (of mousedown) was fired on that div.
             });
             box.addEventListener('click', (e)=>{
                 clicked(e.target);
+                //handles colouring of simple clicked div.
             })
         }
         grid.appendChild(column);
@@ -36,7 +40,7 @@ function createGrid(cellNum){
 }
 
 function hovered(item){
-    if(mouseDown)
+    if(mouseDown) //colours only if mouseDown is true, aka mousedown event was fired on target div.
         item.classList.add("hovered");
 }
 
@@ -44,8 +48,18 @@ function clicked(item){
     item.classList.add("hovered");
 }
 
-function print(num){
-    console.log("hello "+num);
+let input = document.getElementById("grid-size");
+let resizeButton = document.getElementById("resize-button");
+
+
+resizeButton.addEventListener('click', () => {
+    gridSize = input.value;
+    newGrid(gridSize);
+});
+
+function newGrid(gridSize){
+    grid.innerHTML = "";
+    createGrid(gridSize);
 }
 
-createGrid(10);
+createGrid(5);
