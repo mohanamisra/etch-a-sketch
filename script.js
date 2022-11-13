70a38ca045c5cfd6468b6b776efee097f1c45ffb
Mohana Misra <iammohanamisra@gmail.com>
2022-11-13 16:17:53 +0530

    Added clear button

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ let mouseDown = false;
 //variables for functioning of the page
 let grid = document.getElementsByClassName("grid")[0];
 let resizeButton = document.getElementById("resize-button");
+let clearButton = document.getElementById("clear-button");
+let input = document.getElementsByClassName("input-grid-size")[0];
+let gridSize;
 
 createGrid(16); //This is the default grid size when the page loads
+addEventListeners();
 
 //grid resizing according to user input
 resizeButton.addEventListener('click', () =>{
-    let input = document.getElementsByClassName("input-grid-size")[0];
-    let gridSize = input.value;
+    gridSize = input.value;
     if(gridSize > 100 || gridSize < 1){
         alert("Invalid Input: Please enter a size between 1 and 100")
     }
@@ -22,8 +25,15 @@ resizeButton.addEventListener('click', () =>{
     else{
         grid.innerHTML = ""; //Otherwise previous grids remain and clutter
         createGrid(gridSize);
+        addEventListeners();
     }
 });
+
+clearButton.addEventListener('click', ()=>{
+    grid.innerHTML = "";
+    createGrid(gridSize);
+    addEventListeners();
+})
 
 
 //grid resizing/creating function
@@ -40,25 +50,28 @@ function createGrid(gridSize){
     }
 }
 
-let boxes = document.getElementsByClassName("box");
-for(let eachBox of boxes){
-    eachBox.addEventListener('mousedown', (e)=>{
-        e.preventDefault();
-        mouseDown = true;
-    });
-    eachBox.addEventListener('mouseenter', (e) => {
-        if(mouseDown){
+
+function addEventListeners(){
+    let boxes = document.getElementsByClassName("box");
+    for(let eachBox of boxes){
+        eachBox.addEventListener('mousedown', (e)=>{
+            e.preventDefault();
+            mouseDown = true;
+        });
+        eachBox.addEventListener('mouseenter', (e) => {
+            if(mouseDown){
+                let boxToColour = e.target;
+                colour(boxToColour);
+            }
+        });
+        eachBox.addEventListener('mouseup', (e)=>{
+            mouseDown = false;
+        });
+        eachBox.addEventListener('click', (e)=>{
             let boxToColour = e.target;
             colour(boxToColour);
-        }
-    });
-    eachBox.addEventListener('mouseup', (e)=>{
-        mouseDown = false;
-    });
-    eachBox.addEventListener('click', (e)=>{
-        let boxToColour = e.target;
-        colour(boxToColour);
-    })
+        })
+    }
 }
 
 function colour(boxToColour){
