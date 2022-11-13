5ff20d804749947cb9df66c8ea94b9f154c401e5
Mohana Misra <iammohanamisra@gmail.com>
2022-11-13 13:55:13 +0530

    Added colour buttons html

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let grid = document.getElementsByClassName("grid")[0];
 let box;
-
-for(let i = 1; i <= 16; i++){
-    let column = document.createElement("div");
-    column.classList.add("column");
-    for(let j = 1; j <= 16; j++){
-        box = document.createElement("div");
-        box.classList.add("box");
-        column.appendChild(box);
+function createGrid(boxNum){
+    for(let i = 1; i <= boxNum; i++){
+        let column = document.createElement("div");
+        column.classList.add("column");
+        for(let j = 1; j <= boxNum; j++){
+            box = document.createElement("div");
+            box.classList.add("box");
+            column.appendChild(box);
+        }
+        grid.appendChild(column);
     }
-    grid.appendChild(column);
 }
+
+createGrid(16);
